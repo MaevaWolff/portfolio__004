4a06c7c51abdea3ed49804baa40bbee9b8aeac3b
Maeva Wolff <maeva.wolff@hetic.net>
2020-05-06 21:17:06 +0200

    add loader to home page

---
+++ src/js/loader.js
@@ -0,0 +1,21 @@
+const $homepage = document.getElementById("home");
+const $loader = document.querySelector(".loader");
+const $containerTyping = document.querySelector(".loader__typing");
+const text = "Maëva WOLFF - Portfolio 2020";
+let i = 0;
+let letter = "";
+
+function typing() {
+  $containerTyping.style.opacity = "1";
+  letter = text.slice(0, ++i);
+  $containerTyping.textContent = letter;
+  setTimeout(typing, 50);
+}
+
+function showHome() {
+  $loader.style.display = "none";
+  $homepage.style.display = "block";
+}
+
+typing();
+setTimeout(showHome, 3000);
