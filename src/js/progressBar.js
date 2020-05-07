3e2ac157dcaed4f2daf744edf9f66cae1a382215
Maeva Wolff <maeva.wolff@hetic.net>
2020-05-07 20:29:20 +0200

    :construction: :poop: test progress bar in works page

---
+++ src/js/progressBar.js
@@ -0,0 +1,18 @@
+const $progressBar = document.querySelector(".progress__bar");
+const $buttonNext = document.querySelector(".next");
+const $buttonPrev = document.querySelector(".prev");
+let width = window.innerWidth;
+
+const $containerWorks = document.querySelector(".worksWrapper");
+const test = $containerWorks.offsetLeft;
+
+console.log(test);
+
+// function addProgress() {
+
+//   }
+// }
+
+// $buttonNext.addEventListener("click", () => {
+//   addProgress();
+// });
