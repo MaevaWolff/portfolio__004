e9af719be192437730514cb6162ebdab3db0ddf4
Maeva Wolff <maeva.wolff@hetic.net>
2020-05-08 22:50:25 +0200

    add jquery for works filter

--- src/js/choiceWorks.js
+++ src/js/choiceWorks.js
@@ -1,4 +1,5 @@
 const $choiceType = document.querySelectorAll(".works__choice li");
+const dataSelector = "";
 
 let choiceSelected = () => {
   for (let i = 0; i < $choiceType.length; i++) {
@@ -12,3 +13,17 @@ let choiceSelected = () => {
 };
 
 choiceSelected();
+
+// filter data
+
+$($choiceType).click(function () {
+  value = $(this).data("filter");
+  $($choiceType).removeClass("active"), $(this).addClass("active");
+
+  $(".work").each(function () {
+    el = $(this);
+
+    el.show();
+    if (!el.hasClass(value) && value != "all") el.hide();
+  });
+});
